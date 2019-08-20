eea544be2ed5040574d505c6e9ed4e8038317edc
mkifetew2 <mkifetew@ryerson.ca>
2019-08-20 12:36:40 -0400

    User interface updated, as well as functional API calls now

--- instantWeather/instantWeather/Controllers/CityNameViewController.swift
+++ instantWeather/instantWeather/Controllers/CityNameViewController.swift
@@ -28,6 +28,13 @@ class CityNameViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view.
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (cityEntry.text?.count)! > 0
+        {
+            submitButton.isEnabled = true
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -44,75 +51,22 @@ class CityNameViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    
+    
     @IBAction func weatherButtonPressed(_ sender: UIButton)
     {
         let cityName = String(cityEntry.text!)
-        let params : [String : String] = ["q" : cityName, "appid" : APP_ID]
-        getWeather(url: APP_URL, params: params)
-    }
-    
-    func getWeather(url: String, params: [String: String])
-    {
-        Alamofire.request(url, method: .get, parameters: params).responseJSON
-            {
-                response in
-                if response.result.isSuccess
-                {
-                    print("Success")
-                    let weatherData : JSON = JSON(response.result.value!)
-                    print(weatherData)
-                    self.weatherUI(json: weatherData)
-                }
-                    
-                else
-                {
-                    print("Error \(response.result.error!)")
-                }
-        }
-    }
-    
-    func weatherUI(json : JSON)
-    {
-        let temp = json["main"]["temp"].int
-        let id = json["weather"][0]["id"].int
-        cityTemp = temp! - 273
-//        print("This is city temp: \(cityTemp!)")
-//        print("This is the temperature:  \(temp!)")
-//        print("This is the description: \(description!)")
-//        print("This is the ID: \(id!)")
-        weatherDisplay(id: id!)
-        let nextScreen = WeatherDisplayViewController()
-        nextScreen.cityName = cityEntry.text!
-        
         
     }
     
-    
-    func weatherDisplay(id: Int)
-    {
-        switch id
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dstVC = segue.destination as? WeatherDisplayViewController
         {
-            case 200, 201, 202, 210, 211, 212, 221, 230, 231, 232:
-                desc = "thunderstorm"
-            case 300, 301, 302, 310, 311, 312, 313, 314, 321:
-                desc = "drizzle"
-            case 500, 501, 502, 503, 504, 511, 520, 521, 522, 531:
-                desc = "rain"
-            case 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622:
-                desc = "snow"
-            case 701, 711, 721, 731, 741, 751, 761, 762, 771, 781:
-                desc = "atmosphere"
-            case 800:
-                desc = "clear sky"
-            case 801, 802, 803, 804:
-                desc = "clouds"
-            default:
-                desc = "none"
+            dstVC.cityName = cityEntry.text!
         }
-        print(desc!)
-        
-        
     }
+    
+    
     
     
 }
--- instantWeather/instantWeather/Controllers/WeatherDisplayViewController.swift
+++ instantWeather/instantWeather/Controllers/WeatherDisplayViewController.swift
@@ -12,36 +12,101 @@ import SwiftyJSON
 
 class WeatherDisplayViewController: UIViewController {
 
-    var cityName : String? = ""
-    var weatherDescription : String? = ""
+    var cityName : String?
+    var weatherDescription : String?
     var cityTemp : Int? = 0
     @IBOutlet weak var weatherDisplay: UIImageView!
     @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    var APP_URL = "http://api.openweathermap.org/data/2.5/weather"
+    var APP_ID = "993d139627c4a431d89b8b944c0ea92e"
     
     @IBAction func switchPage(_ sender: UIButton)
     {
-        
+        print(cityName)
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let params : [String : String] = ["q" : cityName!, "appid" : APP_ID]
+        getWeather(url: APP_URL, params: params)
+        print("here")
         
     }
     
-    override func viewDidAppear(_ animated: Bool)
-    {
-        
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func getWeather(url: String, params: [String: String])
+    {
+        Alamofire.request(url, method: .get, parameters: params).responseJSON
+            {
+                response in
+                if response.result.isSuccess
+                {
+                    print("Success")
+                    let weatherData : JSON = JSON(response.result.value!)
+                    print(weatherData)
+                    self.weatherUI(json: weatherData)
+                }
+                    
+                else
+                {
+                    print("Error \(response.result.error!)")
+                }
+        }
+    }
+    
+    func weatherUI(json : JSON)
+    {
+        let temp = json["main"]["temp"].int
+        let id = json["weather"][0]["id"].int
+        
+        if temp != nil
+        {
+            cityTemp = temp! - 273
+        }
+        weatherDisplay2(id: id!)
+        
+    }
 
-   
+    
+    func weatherDisplay2(id: Int)
+    {
+        switch id
+        {
+        case 200, 201, 202, 210, 211, 212, 221, 230, 231, 232:
+            weatherDescription = "thunderstorm"
+            weatherDisplay.image = UIImage(named: "thunderstorm")
+        case 300, 301, 302, 310, 311, 312, 313, 314, 321:
+            weatherDescription = "drizzle"
+            weatherDisplay.image = UIImage(named: "rainy")
+        case 500, 501, 502, 503, 504, 511, 520, 521, 522, 531:
+            weatherDescription = "rain"
+            weatherDisplay.image = UIImage(named: "rainy2")
+        case 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622:
+            weatherDescription = "snow"
+            weatherDisplay.image = UIImage(named: "lightSnow")
+        case 701, 711, 721, 731, 741, 751, 761, 762, 771, 781:
+            weatherDescription = "atmosphere"
+            weatherDisplay.image = UIImage(named: "fog")
+        case 800:
+            weatherDescription = "clear sky"
+            weatherDisplay.image = UIImage(named: "verySunny")
+        case 801, 802, 803, 804:
+            weatherDescription = "clouds"
+            weatherDisplay.image = UIImage(named: "cloudy")
+        default:
+            weatherDescription = "none"
+        }
+        
+        tempLabel.text = "\(cityTemp!)℃"
+        cityLabel.text = cityName!
+        
+        
+    }
 
 }
